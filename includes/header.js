d04983bb4715cca6252df2be5c1be039af5a0963
kalculata <nprincehuzaifa@gmail.com>
2022-02-03 10:08:33 +0200

    make header incluables

---
+++ includes/header.js
@@ -0,0 +1,17 @@
+Vue.component('c-header', {
+    template: `
+    <header>
+        <div class="arrow-left"><a href="https://www.Kubwacu.com/"><i class="fas fa-arrow-left"></i></a></div>
+        <div class="logo">
+            <div class="logo__container">
+                <div class="logo__svg"><img src="src/images/logo.svg" alt="Kubwacu logo"></div>
+                <div class="logo__compl">
+                    <span class="logo__prefix">ubwacu</span>
+                    <span class="logo__suffix">wiga</span>
+                </div>   
+            </div>
+        </div>
+        <div class="nav-bar"><i class="fas fa-bars"></i></div>
+    </header>
+    `,
+})
