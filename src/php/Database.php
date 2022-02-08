6cb13e776928b842133cb805cef033f211127673
Hafgun <hafpixels06@gmail.com>
2022-02-08 10:20:35 +0200

    set a unique connexion

--- src/php/Database.php
+++ src/php/Database.php
@@ -3,22 +3,26 @@
 
 class Database
 {
+    
     /**
-     * Retourne connexion à la base de donées
-     * 
-     * @return PDO
+     * Retourne une seule connexion à la base de donées
      */
+    
+    private static $instance = null;
+
     public static function getPdo(): PDO
     {
         $HOST= "localhost"; 
-        $DATABASE ="kubwacu_twige"; 
+        $DATABASE ="blogpoo"; 
         $USER = "root"; 
         $PASSWORD = "";
-        $pdo = new PDO('mysql:host='.$HOST.';dbname='.$DATABASE.';charset=utf8', $USER, $PASSWORD, [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ]);
+        if (self::$instance === null) {
+            self::$instance  = new PDO('mysql:host='.$HOST.';dbname='.$DATABASE.';charset=utf8', $USER, $PASSWORD, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]);
+        }
 
-        return $pdo;
+        return self::$instance;
     }
 }
