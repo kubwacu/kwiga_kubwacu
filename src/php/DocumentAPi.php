054730c83c723f00e6b3446f35067873aa5582a2
Hafgun <hafpixels06@gmail.com>
2022-02-08 21:45:19 +0200

    is the DocumentAPI.php file. ps: have not worked on it for now

--- src/php/DocumentAPi.php
+++ src/php/DocumentAPi.php
@@ -2,6 +2,12 @@
 require_once 'Database.php';
 
 class DocumentAPI{
+
+    public function __construct()
+    {
+        $this->pdo =
+         \Database::getPdo();
+    }
     public static function generate_doc_title($year, $type, $cours, $section_fac=null, $departement=null, $university=null, $stage=null): string{
         /*
             examples of returns:
@@ -14,13 +20,23 @@ class DocumentAPI{
             - generate_doc_title("2020-2021", "examen-uni", "PHP", "Info", "Genie logiciel", "Université du lac tanganyika", 3) 
                 => "PHP - Examen pour faculté Info-département Génie logiciel BAC3 à l'université du lac Tanganyika année academique 2020-2021"
         */
+        /*
+        1. vérifier si tous les cases sont complétés selon le type de document
+        */
+        /*
+        2. Emmetre une erreur pour chaque cas
+        */
+        /*
+        3. ecrire les données dans le titre selon l'exemple
+        */
+        
     }
-    public static function save_document($path, $year, $type, $cours, $section_fac, $departement, $university, $stage): void{} //question sur l'enregistrement
+    /*public static function save_document($path, $year, $type, $cours, $section_fac, $departement, $university, $stage): void{} //question sur l'enregistrement
     public static function get_document($id): array{} //Done. It is in Model file and is called "find($id)"
     public static function get_last_added($nbr): array{} //Done It is in Model file and is called "findLastAdded($number)"
     public static function search($q): array{}
     public static function get_concours_9($edition=null): array{}
     public static function get_exetat($section=null, $session=null): array{}
-    public static function get_examen_uni($university=null, $fac=null, $dep=null, $stage=null, $academic_year=null): array{}
+    public static function get_examen_uni($university=null, $fac=null, $dep=null, $stage=null, $academic_year=null): array{}*/
 
 }
