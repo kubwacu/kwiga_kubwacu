dc051eb537efd0d54f48574bfa63948776b87f27
Hafgun <hafpixels06@gmail.com>
2022-02-09 20:38:06 +0200

    added year dropdown list depending on the type

--- includes/dropdown.js
+++ includes/dropdown.js
@@ -1,19 +1,66 @@
-function showHideInput(sel){
+
+//const typeSelect = document.getElementById('type');
+
+function showHideInput(sel,yearSelect){
     var value = sel.value;
+    var yearSelect = document.getElementById('year');
+    //get the current year
+    let year = new Date().getFullYear();
+
     if(value===""){
+        //hide details
         document.getElementById('exetatsOp').style.display = 'none';
         document.getElementById('universityOp').style.display = 'none';
+        //delete all the content in year select
+        while(yearSelect.firstChild){
+            yearSelect.removeChild(yearSelect.firstChild);
+        }
     }
     else if(value==="concours-9"){
+        //hide details exetat and university inputs
         document.getElementById('exetatsOp').style.display = 'none';
         document.getElementById('universityOp').style.display = 'none';
+        //delete all the content in year select
+        while(yearSelect.firstChild){
+            yearSelect.removeChild(yearSelect.firstChild);
+        }
+        //show the right year select options        
+        for (let index = 0; index < 30; index++) {
+            const option = document.createElement("option");
+            option.textContent = year-index;
+            yearSelect.appendChild(option);         
+        }    
     }
     else if(value==="exetat"){
+        //show exetat and hide university inputs
         document.getElementById('exetatsOp').style.display = 'block';
         document.getElementById('universityOp').style.display = 'none';
+        //delete all the content in year select
+        while(yearSelect.firstChild){
+            yearSelect.removeChild(yearSelect.firstChild);
+        }
+        //show the right year select options         
+        for (let index = 0; index < 30; index++) {
+            const option = document.createElement("option");
+            option.textContent = year-index;
+            yearSelect.appendChild(option);        
+        }    
     }
     else{
         document.getElementById('exetatsOp').style.display = 'none';
         document.getElementById('universityOp').style.display = 'block';
+        //delete all the content in year select
+        while(yearSelect.firstChild){
+            yearSelect.removeChild(yearSelect.firstChild);
+        }
+        //show the right year select options         
+        for (let index = 0; index < 11; index++) {
+            const option = document.createElement("option");
+            option.textContent = toString(year-index,year+1-index) ;
+            yearSelect.appendChild(option);       
+        }    
     }
+}
+function toString(a,b){
+    return a+'-'+b;
 }
