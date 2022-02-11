f67e9afec3681c19bf4f91d5f42b070090bb33f4
Hafgun <hafpixels06@gmail.com>
2022-02-11 22:21:48 +0200

    deleted unused files in this project

--- src/php/autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-spl_autoload_register(function($className){
-    //Controllers\DocumentAPI
-    //require = libraries/controllers/DocumentAPI.php
-    $className = str_replace("\\","/",$className);
-    require_once("src/php/$className.php");
-});
