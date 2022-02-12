38bddffb29f99cd884ef9cc5adb82d71a2a56fb2
Hafgun <hafpixels06@gmail.com>
2022-02-12 14:00:16 +0200

    some changes.

--- includes/ajax.js
+++ includes/ajax.js
@@ -1,20 +1,21 @@
 $(document).ready(function(){
 
-    var type = $('#type').val();
-    var cours = $('#cours').val();
-    var year = $('#year').val();
-    var section = $('#section').val();
-    var fac = $('#fac').val();
-    var departement = $('#departement').val();
-    var université = $('#université').val();
-    var stage = $('#stage').val();
+    var type = $.trim($('#type').val());
+    var cours = $.trim($('#cours').val());
+    var year = $.trim($('#year').val());
+    var section = $.trim($('#section').val());
+    var fac = $.trim($('#fac').val());
+    var departement = $.trim($('#departement').val());
+    var université = $.trim($('#université').val());
+    var stage = $.trim($('#stage').val());
 
     var complete = true;
     if (type ==="" || cours ==="" || year === "" || section ==="" || fac==="" || departement==="" || université==="" || stage==="") {
         complete =false;
     }
     if (complete) {
-        $('#submit').click(function(){
+        $('#submit').click(function(e){
+            e.preventDefault();
             formSubmit();
         });
     }
@@ -32,29 +33,31 @@ function formSubmit(){
     var departement = $('#departement').val();
     var université = $('#université').val();
     var stage = $('#stage').val();
-    
+
     //var document = $('#documentFile').val();
 
     //récupération du de la méthode
     var methode = "save";
     //récupération de données selon les types
     if(type==="concours-9"){
-        var data = {methode: methode, type: type, cours: cours, year: year};
+        var data = "{'methode':'"+ methode+"', 'type ': '"+type+"', 'cours:'"+ cours+"',' year':'"+ year+"'}";
     }
     if(type==="exetat"){
-        var data = {methode: methode, type: type, cours: cours, year: year, section: section};
+        var data = "{'methode':'"+ methode+"', 'type ': '"+type+"', 'cours:'"+ cours+"',' year':'"+ year+"', 'section':'" +section+"'}";
     }
     if(type==="examen-uni"){
-        var data = {methode: methode, type: type, cours: cours, year: year, fac: fac, departement: departement, université: université, stage: stage}; 
+        var data = "{'methode':'"+ methode+"', 'type ': '"+type+"', 'cours:'"+ cours+"',' year':'"+ year+"',' fac':'"+ fac+"',' departement':'" +departement+"',' université': '"+université+"',' stage':'"+ stage+"'}"; 
     }
     $.ajax({
         cache:false,
-        url:"http://localhost/kwiga_kubwacu/src/php/DocumentApi.php",
+        url:"src/php/DocumentApi.php",
         data: data,
-        type: "post",
-        dataType ="text",
+        contentType: "application/json; charset=utf-8",
+        type: $('#fom').attr('method'),
+        dataType : "json",
         success: function(response){
-            console.log(response);
+            console.log(response)
+            
         }
     });
 }
